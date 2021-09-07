8e75ae50b4f6e30b54f86af07ddaed86113eb3f7
Olivia Bodén <Olivia.annemaria@hotmail.com>
2021-09-07 08:31:16 +0200

    file fix

---
+++ src/components/navbar/shoppingcart-btn/cartButton.tsx
@@ -0,0 +1,22 @@
+import React, {FC, MouseEventHandler, CSSProperties} from "react";
+import './cartButton.css';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { Link } from "react-router-dom";
+
+const CartButton = () => {
+
+    return (
+        <button className= "cart-btn" >
+            <Link to="/checkout" style={noLinkStyle}>
+            <ShoppingCartIcon className= "shoppingcart" fontSize="small" ></ShoppingCartIcon>
+            </Link>
+        </button>
+        
+    );
+}
+
+const noLinkStyle: CSSProperties = {
+    textDecoration: 'none'
+}
+
+export default CartButton;
