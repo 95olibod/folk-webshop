3e0a97d0d830918461040455399dde246f14702f
Olivia Bodén <Olivia.annemaria@hotmail.com>
2021-09-07 08:33:16 +0200

    file fix

---
+++ src/components/movie/button/button.tsx
@@ -0,0 +1,22 @@
+import React, { FC, MouseEventHandler } from "react";
+//import './cart-btn.css';
+import Button from "@material-ui/core/Button";
+
+interface Props {
+  onClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+const AddCartButton: FC<Props> = ({ onClick }) => {
+  return (
+    <Button
+      className="shoppingcart"
+      variant="contained"
+      color="primary"
+      onClick={onClick}
+    >
+      Lägg i varukorg
+    </Button>
+  );
+};
+
+export default AddCartButton;
