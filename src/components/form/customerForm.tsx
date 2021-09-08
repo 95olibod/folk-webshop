6f7abeca34c0b0fc21f2cca7c598a098e57dcdcc
Olivia Bodén <Olivia.annemaria@hotmail.com>
2021-09-08 11:48:22 +0200

    input form with autofill

--- src/components/form/customerForm.tsx
+++ src/components/form/customerForm.tsx
@@ -1,22 +1,24 @@
-import Paper from '@material-ui/core/Paper';
-import {
-    makeStyles,
-    withStyles
-} from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
-import React from 'react';
+import React from "react";
 import "./customerForm.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
     flexWrap: "wrap",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
-      paper: {
-      padding: theme.spacing(2),
-      margin: 'auto',
-      maxWidth: 500,
-    },
+  paper: {
+    alignItems: "center",
+    padding: theme.spacing(2),
+    margin: "auto",
+    maxWidth: 530,
+    marginBottom: "3rem",
+  },
   margin: {
     margin: theme.spacing(1),
   },
@@ -46,52 +48,68 @@ const CustomerForm = () => {
 
   return (
     <div>
-    <Paper className={classes.paper}>
-      <form className={classes.root} noValidate>
-        <ValidationTextField
-          className={classes.margin}
-          label="Namn"
-          required
-          variant="outlined"
-          id="validation-outlined-input"
-        />
-        <ValidationTextField
-          className={classes.margin}
-          label="Efternamn"
-          required
-          variant="outlined"
-          id="validation-outlined-input"
-        />
-        <ValidationTextField
-          className={classes.margin}
-          type="email"  
-          label="Email"
-          required
-          variant="outlined"
-          id="validation-outlined-input"
-        />
-        <ValidationTextField
-          className={classes.margin}
-          label="Mobilnummer"
-          required
-          variant="outlined"
-          id="validation-outlined-input"
-        />
-        <ValidationTextField
-          className={classes.margin}
-          label="Postnummer"
-          required
-          variant="outlined"
-          id="validation-outlined-input"
-        />
-        <ValidationTextField
-          className={classes.margin}
-          label="Address"
-          required
-          variant="outlined"
-          id="validation-outlined-input"
-        />
-      </form>
+      <Paper className={classes.paper}>
+        <h4>Dina uppgifter</h4>
+
+        <form className={classes.root} noValidate autoComplete="on">
+          <ValidationTextField
+            className={classes.margin}
+            label="För- & Efternamn"
+            name="name"
+            required
+            variant="outlined"
+            id="validation-outlined-input"
+            autoComplete="name"
+          />
+
+          <ValidationTextField
+            className={classes.margin}
+            label="Adress"
+            name="ship-address"
+            required
+            variant="outlined"
+            id="validation-outlined-input"
+            autoComplete="shipping street-address"
+          />
+
+          <ValidationTextField
+            className={classes.margin}
+            label="Postnummer"
+            name="ship-zip"
+            required
+            variant="outlined"
+            id="validation-outlined-input"
+            autoComplete="shipping postal-code"
+          />
+          <ValidationTextField
+            className={classes.margin}
+            label="Stad"
+            name="ship-city"
+            required
+            variant="outlined"
+            id="validation-outlined-input"
+            autoComplete="shipping locality"
+          />
+          <ValidationTextField
+            className={classes.margin}
+            type="email"
+            label="Email"
+            name="email"
+            required
+            variant="outlined"
+            id="validation-outlined-input"
+            autoComplete="email"
+          />
+          <ValidationTextField
+            className={classes.margin}
+            label="Mobilnummer"
+            name="mobile"
+            required
+            variant="outlined"
+            id="validation-outlined-input"
+            autoComplete="tel"
+          />
+        </form>
       </Paper>
     </div>
   );
