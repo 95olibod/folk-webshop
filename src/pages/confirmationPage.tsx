545de220a27ac38bed2511565d6058b715507dd2
Milbredt <55477642+Milbredt@users.noreply.github.com>
2021-09-13 14:09:23 +0200

    origin/feature/51-fix-confirmation-page

--- src/pages/confirmationPage.tsx
+++ src/pages/confirmationPage.tsx
@@ -1,7 +1,6 @@
 import { CSSProperties, useContext } from "react";
 import { MovieData, movies } from "../components/movie/movie";
 import { MovieContext } from "../contexts/movieContext";
-import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -10,138 +9,132 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 
-interface Props {
-    movie: MovieData;
-}
-
 const ConfirmationPage = () => {
-    const { addedMovies } = useContext(MovieContext);
+  const { addedMovies } = useContext(MovieContext);
 
-    const AddedMoviesList = movies.filter((movie) =>
-        addedMovies.includes(movie.id)
-    );
+  const AddedMoviesList = movies.filter((movie) =>
+    addedMovies.includes(movie.id)
+  );
 
-    let newArray: MovieData[] = [];
+  let newArray: MovieData[] = [];
 
-    for (const item of addedMovies) {
-        const arr = AddedMoviesList.find((movie) => item === movie.id);
-        if (arr) {
-            newArray.push(arr);
-        }
+  for (const item of addedMovies) {
+    const arr = AddedMoviesList.find((movie) => item === movie.id);
+    if (arr) {
+      newArray.push(arr);
     }
+  }
 
-    console.log(newArray);
-    //Töm local storage
+  //Töm local storage
 
-    const useStyles = makeStyles({
-        table: {
-            minWidth: 700,
-        },
-    });
+  const calculateSumPerMovie = (id: number, price: number) => {
+    const quantity = newArray.filter((movie) => movie.id === id).length;
+    const sum = quantity * price;
 
-    const test = () => {
-        const quantity = newArray.filter((movie) => movie.id === id).length;
-        const sum = quantity * price;
-        // console.log(id);
-        console.log(quantity);
+    return sum;
+  };
 
-    
-    }
+  const getQuantityPerMovie = (id: number) => {
+    const quantity = newArray.filter((movie) => movie.id === id).length;
 
-    function priceRow(qty: number, unit: number) {
-        return qty * unit;
-    }
+    return quantity;
+  };
 
-    function createRow(desc: string, qty: number, unit: number) {
-        const price = priceRow(qty, unit);
-        return { desc, qty, unit, price };
-    }
+  const subtotal = newArray
+    .map(({ price }) => price)
+    .reduce((sum: number, i: number) => sum + i, 0);
 
-    // function subtotal(AddedMoviesList:MovieData) {
-    //     return AddedMoviesList
-    //         .map(({ price }) => price)
-    //         .reduce((sum: number, i: number) => sum + i, 0);
-    // }
-
-    // const rows = [
-    //     createRow("Paperclips (Box)", 100, 1.15),
-    //     createRow("Paper (Case)", 10, 45.99),
-    //     createRow("Waste Basket", 2, 17.99),
-    // ];
-
-    // const invoiceSubtotal = subtotal(rows);
-    // const invoiceTaxes = TAX_RATE * invoiceSubtotal;
-    // const invoiceTotal = invoiceTaxes + invoiceSubtotal;
-
-    const classes = useStyles();
-
-    //summering av order
-    //Tack för beställning
-    return (
-        <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="spanning table">
-                <TableHead>
-                    {/* <TableRow>
-                        <TableCell align="center" colSpan={3}>
-                            Details
-                        </TableCell>
-                        <TableCell align="right">Price</TableCell>
-                    </TableRow> */}
-                    <TableRow>
-                        <TableCell>Titel</TableCell>
-                        <TableCell align="right">Antal</TableCell>
-                        <TableCell align="right">Pris</TableCell>
-                        <TableCell align="right">Summa</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {AddedMoviesList.map((movie) => (
-                        <TableRow key={movie.id}>
-                            <TableCell>{movie.title}</TableCell>
-                            <TableCell align="right">{movie.id}</TableCell>
-                            <TableCell align="right">{movie.price}</TableCell>
-                            <TableCell align="right">
-                                {ccyFormat(movie.id, movie.price)}
-                            </TableCell>
-                        </TableRow>
-                    ))}
-
-                    <TableRow>
-                        <TableCell colSpan={2}>Total</TableCell>
-                        <TableCell align="right">
-                            {/* {ccyFormat(invoiceTotal)} */}
-                        </TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
+  return (
+    <div style={confirmationContainer}>
+      <div style={padding}>
+        <div style={confirmationInformationContainer}>
+          <p style={confirmText}>
+            Tack för din order! Vi har skickat en orderbekräftelse till
+            kalle.hansson.blomberg@hotmail.com
+          </p>
+        </div>
+        <TableContainer style={table} component={Paper}>
+          <Table aria-label="spanning table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Titel</TableCell>
+                <TableCell align="right">Antal</TableCell>
+                <TableCell align="right">Pris</TableCell>
+                <TableCell align="right">Summa</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {AddedMoviesList.map((movie) => (
+                <TableRow key={movie.id}>
+                  <TableCell>{movie.title}</TableCell>
+                  <TableCell align="right">
+                    {getQuantityPerMovie(movie.id)}
+                  </TableCell>
+                  <TableCell align="right">{movie.price}</TableCell>
+                  <TableCell align="right">
+                    {calculateSumPerMovie(movie.id, movie.price)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </TableContainer>
-    );
+        <TableContainer style={table} component={Paper}>
+          <Table aria-label="spanning table">
+            <TableBody>
+              <TableRow>
+                <TableCell style={totalBold} colSpan={3}>
+                  Totalsumma
+                </TableCell>
+                <TableCell style={totalBold} align="right">
+                  {subtotal} kronor
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
 };
 
-const dataGrid: CSSProperties = {
-    color: "white",
-    display: "flex",
-    justifyContent: "flex-start",
+const confirmationContainer: CSSProperties = {
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  marginBottom: "0.5rem",
+  position: "absolute",
+  top: "6rem",
 };
-const style: CSSProperties = {
-    display: "flex",
-    justifyContent: "center",
-    color: "white",
-    alignItems: "center",
+const confirmText: CSSProperties = {
+  display: "flex",
+  flexWrap: "wrap",
+  textAlign: "center",
+  margin: "0.5rem",
 };
 
-const confirmationObject: CSSProperties = {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    color: "white",
-    alignItems: "center",
-    backgroundColor: "red",
+const confirmationInformationContainer: CSSProperties = {
+  display: "flex",
+  backgroundColor: "white",
+  flexGrow: 1,
+  maxWidth: "40rem",
+  borderRadius: "4px",
 };
-const confirmationListStyle: CSSProperties = {
-    listStyleType: "none",
-    display: "flex",
-    width: 200,
+
+const table: CSSProperties = {
+  display: "flex",
+  flexGrow: 1,
+  maxWidth: "40rem",
+  marginTop: "0.5rem",
+};
+
+const totalBold: CSSProperties = {
+  fontWeight: "bold",
+};
+
+const padding: CSSProperties = {
+  padding: "0.5rem",
 };
 
 export default ConfirmationPage;
