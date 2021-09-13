3605679abdfb31cd719e8653858729f0153b5bff
Milbredt <55477642+Milbredt@users.noreply.github.com>
2021-09-13 15:36:12 +0200

    origin/feature/57-fix-go-back-to-frontpage-button

--- src/pages/detailsPage.tsx
+++ src/pages/detailsPage.tsx
@@ -1,32 +1,31 @@
 import MovieDetailCard from "../components/movie/movieDetailCard";
 import { MovieData, movies } from "../components/movie/movie";
 import { RouteComponentProps } from "react-router-dom";
-import { CSSProperties, Fragment } from "react";
+import { CSSProperties } from "react";
 
 interface Props {
-    movie: MovieData;
+  movie: MovieData;
 }
 
 interface Props extends RouteComponentProps<{ id: string }> {}
 
 const DetailsPage = ({ match }: Props) => {
-    const id = parseInt(match.params.id);
-    const movieItem = movies.filter((movieItem) => movieItem.id === id);
+  const id = parseInt(match.params.id);
+  const movieItem = movies.filter((movieItem) => movieItem.id === id);
 
-    return (
-        <div style={style}>
-            {movieItem.map((movie) => (
-                <MovieDetailCard key={movie.id} movie={movie} />
-            ))}            
-        </div>
-    );
+  return (
+    <div style={style}>
+      {movieItem.map((movie) => (
+        <MovieDetailCard key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
 };
 
 const style: CSSProperties = {
-    display: "flex",
-    justifyContent: "center",
-    // top: "5rem",
-   alignItems: "center",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
 };
 
 export default DetailsPage;
