5e10f772c374d8b9c754291f47641384f7759009
liinnjohansson <93linjoh@skola.boras.se>
2021-09-15 20:17:09 +0200

    fixed confirmationPage and frontpage

--- src/pages/confirmationPage.tsx
+++ src/pages/confirmationPage.tsx
@@ -65,55 +65,53 @@ const ConfirmationPage = () => {
   } else {
     return (
       <div className="confirmationContainer">
-        <div className="padding">
-          <div className="confirmationInformationContainer">
-            <p className="confirmText">
-              Tack för din order! Vi har skickat en orderbekräftelse till
-              {" " + userEmail}. Nedan finner du en sammanställning av din order.
-              Välkommen åter!
-            </p>
-          </div>
-          <TableContainer className="table" component={Paper}>
-            <Table aria-label="spanning table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Titel</TableCell>
-                  <TableCell align="right">Antal</TableCell>
-                  <TableCell align="right">Pris</TableCell>
-                  <TableCell align="right">Summa</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {filteredAddedMoviesList.map((movie) => (
-                  <TableRow key={movie.id}>
-                    <TableCell>{movie.title}</TableCell>
-                    <TableCell align="right">
-                      {getQuantityPerMovie(movie.id)}
-                    </TableCell>
-                    <TableCell align="right">{movie.price}</TableCell>
-                    <TableCell align="right">
-                      {calculateSumPerMovie(movie.id, movie.price)}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <TableContainer className="table" component={Paper}>
-            <Table aria-label="spanning table">
-              <TableBody>
-                <TableRow>
-                  <TableCell className="totalBold" colSpan={3}>
-                    Totalsumma
-                  </TableCell>
-                  <TableCell className="totalBold" align="right">
-                    {subtotal} SEK
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
+        <div className="confirmationInformationContainer">
+          <p className="confirmText">
+            Tack för din order! Vi har skickat en orderbekräftelse till
+            {" " + userEmail}. Nedan finner du en sammanställning av din order.
+            Välkommen åter!
+          </p>
         </div>
+        <TableContainer className="table" component={Paper}>
+          <Table aria-label="spanning table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Titel</TableCell>
+                <TableCell align="right">Antal</TableCell>
+                <TableCell align="right">Pris</TableCell>
+                <TableCell align="right">Summa</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {filteredAddedMoviesList.map((movie) => (
+                <TableRow key={movie.id}>
+                  <TableCell>{movie.title}</TableCell>
+                  <TableCell align="right">
+                    {getQuantityPerMovie(movie.id)}
+                  </TableCell>
+                  <TableCell align="right">{movie.price}</TableCell>
+                  <TableCell align="right">
+                    {calculateSumPerMovie(movie.id, movie.price)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TableContainer className="table" component={Paper}>
+          <Table aria-label="spanning table">
+            <TableBody>
+              <TableRow>
+                <TableCell className="totalBold" colSpan={3}>
+                  Totalsumma
+                </TableCell>
+                <TableCell className="totalBold" align="right">
+                  {subtotal} SEK
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     );
   }
