043848461420f89d9805d9420c018a594e9c1249
liinnjohansson <93linjoh@skola.boras.se>
2021-09-16 08:50:00 +0200

    added slogan to homepage

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,11 +1,15 @@
 import { movies } from "../components/movie/movie";
 import MovieCard from "../components/movie/cards/movieCard";
-import "./pages.css"
+import "./pages.css";
+import MainSlogan from "../components/header/mainSlogan";
 
 //Displays all movies
 const HomePage = () => {
   return (
     <div className="outerDiv">
+      <div className="slogan">
+        <MainSlogan />
+      </div>
       <div className="movieContainer">
         {movies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
